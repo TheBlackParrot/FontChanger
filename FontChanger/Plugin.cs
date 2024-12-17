749e5fb35f17851f36a6b5762e66532813a3a8b3
TheBlackParrot <theblackparrot0@gmail.com>
2024-12-17 16:34:58 -0600

    Partially fix soft restarts

--- FontChanger/Plugin.cs
+++ FontChanger/Plugin.cs
@@ -1,10 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using BeatSaberMarkupLanguage.Settings;
 using BeatSaberMarkupLanguage.Util;
 using IPA;
 using FontChanger.Configuration;
+using FontChanger.HarmonyPatches;
 using HarmonyLib;
+using IPA.Config.Data;
 using IPA.Config.Stores;
+using TMPro;
+using UnityEngine;
 using IPALogger = IPA.Logging.Logger;
 using Config = IPA.Config.Config;
 
@@ -40,6 +46,35 @@ namespace FontChanger
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             
             BSMLSettings.Instance.AddSettingsMenu("FontChanger", "FontChanger.UI.BSML.Settings.bsml", Config);
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            TMP_FontAsset TekoFont = Resources.FindObjectsOfTypeAll<TMP_FontAsset>().LastOrDefault(f2 => f2.name == "Teko-Medium SDF");
+            TMP_Text[] textObjs = Resources.FindObjectsOfTypeAll<TMP_Text>();
+            
+            PatcherFunctions.OriginalFontSizes.ForEach(pair =>
+            {
+                if (Resources.InstanceIDIsValid(pair.Key))
+                {
+                    TMP_Text foundObj = textObjs.FirstOrDefault(obj => obj.GetInstanceID() == pair.Key);
+                    if (foundObj != null)
+                    {
+                        if (foundObj.font.name.Contains(Config.FontName))
+                        {
+                            foundObj.font = TekoFont;
+                            foundObj.fontSizeMin = PatcherFunctions.OriginalFontSizeMins.Where(minPair => minPair.Key == pair.Key).First().Value;
+                            foundObj.fontSizeMax = PatcherFunctions.OriginalFontSizeMaxs.Where(maxPair => maxPair.Key == pair.Key).First().Value;
+                            foundObj.fontSize = pair.Value;
+                        }
+                    }
+                }
+            });
+            
+            PatcherFunctions.OriginalFontSizes.Clear();
+            PatcherFunctions.OriginalFontSizeMins.Clear();
+            PatcherFunctions.OriginalFontSizeMaxs.Clear();
         }
 
         [OnExit]
