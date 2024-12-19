21c3e5a9740f98daca1e3788453bcfd1ae410d42
TheBlackParrot <theblackparrot0@gmail.com>
2024-12-19 12:57:12 -0600

    welp i'm away for 4 days now peace

---
+++ FontChanger/HarmonyPatches/BSMLPatches.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Tags;
+using BeatSaberMarkupLanguage.Tags.Settings;
+using BeatSaberMarkupLanguage.TypeHandlers;
+using FontChanger.Configuration;
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+
+// ReSharper disable InconsistentNaming
+
+namespace FontChanger.HarmonyPatches
+{
+    internal class BSMLPatches
+    {
+        [HarmonyPatch]
+        internal class TextTagPatch
+        {
+            private static readonly PluginConfig Config = PluginConfig.Instance;
+
+            [HarmonyPatch(typeof(TextTag), "CreateObject")]
+            [HarmonyPriority(int.MinValue)]
+            [HarmonyFinalizer]
+            internal static void Patch(ref GameObject __result)
+            {
+                PatcherFunctions.Patch(__result.GetComponent<FormattableText>(), Managers.FontManager.Fonts);
+            }
+            
+            [HarmonyPatch(typeof(ClickableTextTag), "CreateObject")]
+            [HarmonyPatch(typeof(SubmenuTag), "CreateObject")]
+            [HarmonyPriority(int.MinValue)]
+            [HarmonyFinalizer]
+            internal static void ClickablePatch(ref GameObject __result)
+            {
+                PatcherFunctions.Patch(__result.GetComponent<ClickableText>(), Managers.FontManager.Fonts);
+            }
+
+            // this crashes
+            [HarmonyPatch(typeof(TextMeshProUGUIHandler), "Setters", MethodType.Constructor)]
+            [HarmonyPriority(int.MinValue)]
+            [HarmonyAfter("com.monkeymanboy.BeatSaberMarkupLanguage")]
+            [HarmonyFinalizer]
+            internal static void PatchValue(Dictionary<string, Action<TextMeshProUGUI, string>> __instance)
+            {
+                foreach (KeyValuePair<string,Action<TextMeshProUGUI,string>> keyValuePair in __instance)
+                {
+                    Plugin.Log.Info($"{keyValuePair.Key}");
+                }
+
+                Plugin.Log.Info("triggered");
+            }
+        }
+    }
+}
